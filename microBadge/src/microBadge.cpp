a2c74c61194bf227755a401baa4ef5703d808755
Chuck Harrison <cfharr@gmail.com>
2021-09-13 11:53:32 -0700

    use require_recipient to test for and notify counterparty (#1 #2 )

--- microBadge/src/microBadge.cpp
+++ microBadge/src/microBadge.cpp
@@ -9,7 +9,7 @@ void microBadge::increment_calls() {
 [[eosio::action]]
 void microBadge::init( name issuer, string badge, name badgee, string memo ) {
     require_auth( issuer );
-    check( is_account( badgee ), "badgee account does not exist");
+    require_recipient( badgee );
     auto calls_row = calls.get();
     calls_row.inits += 1;
     calls_row.all += 1;
@@ -19,28 +19,28 @@ void microBadge::init( name issuer, string badge, name badgee, string memo ) {
 [[eosio::action]]
 void microBadge::issuerupdate( name issuer, string badge, name badgee, string memo ) {
     require_auth( issuer );
-    check( is_account( badgee ), "badgee account does not exist");
+    require_recipient( badgee );
     increment_calls();
 }
 
 [[eosio::action]]
 void microBadge::cancel( name issuer, string badge, name badgee, string memo ) {
     require_auth( issuer );
-    check( is_account( badgee ), "badgee account does not exist");
+    require_recipient( badgee );
     increment_calls();
 }
 
 [[eosio::action]]
 void microBadge::badgeeupdate( name issuer, string badge, name badgee, string memo ) {
     require_auth( badgee );
-    check( is_account( issuer ), "issuer account does not exist");
+    require_recipient( issuer );
     increment_calls();
 }
 
 [[eosio::action]]
 void microBadge::badgeereject( name issuer, string badge, name badgee, string memo ) {
     require_auth( badgee );
-    check( is_account( issuer ), "issuer account does not exist");
+    require_recipient( issuer );
     increment_calls();
 }
 
